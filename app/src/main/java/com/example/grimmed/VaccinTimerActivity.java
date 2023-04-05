6bfaab04c699c16ed42aacd0c103edf8ee5232f1
BENHDACH <mohamed.benhdach@isen.yncrea.fr>
2023-04-05 22:43:24 +0200

    Ajout des vaccins, à remplacer par des buttons et envoyer vers une firebase

--- app/src/main/java/com/example/grimmed/VaccinTimerActivity.java
+++ app/src/main/java/com/example/grimmed/VaccinTimerActivity.java
@@ -4,16 +4,65 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VaccinTimerActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    private List<String> items = new ArrayList<String>();
+    private List<Object> itemsObjets = new ArrayList<>();
+
+    public static List<Object[]> createList(Object[]... elements) {
+        List<Object[]> list = new ArrayList<>();
+        for (Object[] array : elements) {
+            list.add(array);
+        }
+        return list;
+    }
+
+    List<Object[]> mySuperList = createList();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vaccin_timer);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Vaccin Timer");
+        }
+
+        mySuperList.add(new Object[]{"nom du vaccin", "dd/mm/yy", 1});
+
+
+
+
+        initAffichageIcone();
+
+        recyclerView = findViewById(R.id.recyclerVaccin);
+        items.add("A");
+        items.add("O");
+        items.add("I");
+        items.add("E");
+        items.add("Y");
+        itemsObjets.add(new String[]{"nom du vaccin", "dd/mm/yy", "SET?"});
+        VaccinAdapter adapter = new VaccinAdapter(items);
+        recyclerView.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+    }
+
+    public void initAffichageIcone(){
         ImageView buttonMedoc3 = findViewById(R.id.buttonMedoc3);
         buttonMedoc3.setOnClickListener(this::onClick);
 
@@ -26,12 +75,8 @@ public class VaccinTimerActivity extends AppCompatActivity {
         ImageView backHome = findViewById(R.id.backHome);
         backHome.setOnClickListener(this::onClick);
 
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setTitle("Vaccin Timer");
-        }
-
-
+        ImageView plusVaccin = findViewById(R.id.plusVaccinNotif);
+        plusVaccin.setOnClickListener(this::onClick);
     }
 
     public void onClick(View v) {
@@ -53,6 +98,13 @@ public class VaccinTimerActivity extends AppCompatActivity {
         if (v.getId() == R.id.backHome) {
             Intent intent = new Intent(this, BaseActivity.class);
             startActivity(intent);
+        }
+        if (v.getId() == R.id.plusVaccinNotif) {
+
+            VaccinAdapter adapter = new VaccinAdapter(items);
+            recyclerView.setAdapter(adapter);
+
+            adapter.addOne();
         }
     }
 }
