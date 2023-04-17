96170274ffae9d2f5db1b869cf90e030b581b453
colleenDesruisseaux <colleenDesruisseaux>
2023-04-17 16:10:54 +0200

    update des pages connexion et inscription

--- app/src/main/java/com/example/grimmed/PageLogActivity.java
+++ app/src/main/java/com/example/grimmed/PageLogActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class PageLogActivity extends AppCompatActivity {
 
@@ -14,12 +15,19 @@ public class PageLogActivity extends AppCompatActivity {
         setContentView(R.layout.activity_page_log);
 
         Button logPage = findViewById(R.id.logPage);
+        TextView redirection = findViewById(R.id.redirection);
         logPage.setOnClickListener(this::onClick);
+        redirection.setOnClickListener(this::onClick);
     }
 
     public void onClick(View v){
         if (v.getId() == R.id.logPage) {
             Intent intent = new Intent(this, BaseActivity.class);
+            startActivity(intent);
+        }
+
+        if (v.getId() == R.id.redirection) {
+            Intent intent = new Intent(this, PageSignActivity.class);
             startActivity(intent);
         }
     }
