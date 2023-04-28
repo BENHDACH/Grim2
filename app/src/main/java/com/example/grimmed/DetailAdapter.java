886f6d83d175115f8c3bd61da6b720dd4659626b
unknown <MathildeH-28>
2023-04-28 15:49:00 +0200

    début adapter detail, base de la base

--- app/src/main/java/com/example/grimmed/DetailAdapter.java
+++ app/src/main/java/com/example/grimmed/DetailAdapter.java
@@ -1,4 +1,60 @@
 package com.example.grimmed;
 
-public class DetailAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.ViewHolder> {
+
+    private List<String> items;
+
+    public DetailAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titre;
+
+        public ViewHolder(View view) {
+            super(view);
+            titre = view.findViewById(R.id.titre);
+        }
+
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cell_detail, parent, false);
+        return new DetailAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+    }
+
+    // binds the data to the TextView in each row
+    public void onBindViewHolder(VaccinAdapter.ViewHolder holder, int position) {
+        String item = items.get(position);
+        //holder.textView.setText(item);
+        //holder.textSetDateExpi.setText("");
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    void addOne(){
+        List<String> myList = Arrays.asList("L","","","false");
+        items.add(String.format(Locale.getDefault(),"%d",items.size() + 1));
+    }
 }
+
+
