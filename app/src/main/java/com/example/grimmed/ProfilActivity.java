af33c0e8cd3766ab16bf220c571354058a3f25cd
unknown <MathildeH-28>
2023-05-04 09:50:23 +0200

    childactivity

--- app/src/main/java/com/example/grimmed/ProfilActivity.java
+++ app/src/main/java/com/example/grimmed/ProfilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -79,7 +80,7 @@ public class ProfilActivity extends AppCompatActivity {
         }
 
         if (v.getId() == R.id.child) {
-            Intent intent = new Intent(this, DetailActivity.class);
+            Intent intent = new Intent(this, ChildActivity.class);
             startActivity(intent);
         }
     }
