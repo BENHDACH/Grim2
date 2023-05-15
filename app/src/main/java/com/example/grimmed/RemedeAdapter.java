8fc4de821d66701a8226f5ecbed31fe99c41b7f5
unknown <MathildeH-28>
2023-05-15 09:25:30 +0200

    changements pour les remèdes

--- app/src/main/java/com/example/grimmed/RemedeAdapter.java
+++ app/src/main/java/com/example/grimmed/RemedeAdapter.java
@@ -1,15 +1,22 @@
 package com.example.grimmed;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -21,15 +28,21 @@ public class RemedeAdapter extends RecyclerView.Adapter<RemedeAdapter.ViewHolder
 
     private List<JSONArray> itemsSoluce;
 
+    private List<Integer> itemsScore;
 
     private MamieActivity mamieActivity;
 
+    List<List<Object>> combinedList;
 
-    public RemedeAdapter(List<String> items,List<JSONArray> itemsSoluce, List<Boolean> itemsShow, MamieActivity mamieActivity) {
+
+    public RemedeAdapter(List<String> items, List<JSONArray> itemsSoluce, List<Boolean> itemsShow, MamieActivity mamieActivity,
+                         List<Integer> itemsScore, List<List<Object>> combinedList) {
         this.items = items;
         this.itemsSoluce = itemsSoluce;
         this.itemsShow = itemsShow;
         this.mamieActivity = mamieActivity;
+        this.itemsScore = itemsScore;
+        this.combinedList = combinedList;
     }
 
     // inner class to hold a reference to each item view
@@ -39,11 +52,17 @@ public class RemedeAdapter extends RecyclerView.Adapter<RemedeAdapter.ViewHolder
         public TextView textScore;
         private RecyclerView recyler;
 
+        public ImageView plusScore;
+        public ImageView moinScore;
+
+
         public ViewHolder(View view) {
             super(view);
             textView = view.findViewById(R.id.txtSymptome);
             recyler = view.findViewById(R.id.littleRecycler);
             textScore = view.findViewById(R.id.textScore);
+            plusScore = view.findViewById(R.id.plusScore);
+            moinScore = view.findViewById(R.id.moinScore);
         }
 
     }
@@ -55,20 +74,68 @@ public class RemedeAdapter extends RecyclerView.Adapter<RemedeAdapter.ViewHolder
         return new com.example.grimmed.RemedeAdapter.ViewHolder(view);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String item = items.get(position);
+        //String item = items.get(position);
+        String item = (String) combinedList.get(position).get(0);
 
-        Boolean itemBool = itemsShow.get(position);
+        //Boolean itemBool = itemsShow.get(position);
+        Boolean itemBool = (Boolean) combinedList.get(position).get(3);
+
+        String score = combinedList.get(position).get(1).toString();
+
+        Boolean hasVoted = (Boolean) combinedList.get(position).get(4);
+
+        int isVote = (int) combinedList.get(position).get(5);
+
+        Log.e("combined",""+combinedList.get(position));
+
         holder.textView.setText(item);
+
         int myPosition = position;
 
+        //Si l'utilisateur à voté dans cette recette
+        if(hasVoted){
+            //Si il a voté upvote (pouce vert, flèche haut vert/bleu)
+            if(isVote==1){
+                int colorRes = ContextCompat.getColor(mamieActivity, R.color.grimmed);
+                holder.plusScore.setColorFilter(colorRes);
+            }
+            //Si il a voté downvote (pouce rouge, flèche bas rouge)
+            else{
+                //int colorRes = ContextCompat.getColor(mamieActivity, R.color.grimmed);
+                holder.moinScore.setColorFilter(Color.RED);
+            }
+        }
+
+
+
+        //itemsScore.get(position).toString() //Inside the setText()
+        holder.textScore.setText(score);
+
+        //Faire le dérouler en cliquant sur la textView
         holder.textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mamieActivity.onItemClick(myPosition,itemBool);
             }
         });
+
+        //Faire le renouveau du score en cliquant
+        holder.plusScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mamieActivity.onScoreChange(myPosition,1);
+            }
+        });
+        holder.moinScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mamieActivity.onScoreChange(myPosition,-1);
+            }
+        });
+
 
         if(itemBool){
             setInternRecyclerView(holder,myPosition);
@@ -84,7 +151,8 @@ public class RemedeAdapter extends RecyclerView.Adapter<RemedeAdapter.ViewHolder
     // total number of rows
     @Override
     public int getItemCount() {
-        return items.size();
+        return combinedList.size();
+        //return items.size();
     }
 
     private void setInternRecyclerView(ViewHolder holder, int myPosition){
@@ -92,9 +160,12 @@ public class RemedeAdapter extends RecyclerView.Adapter<RemedeAdapter.ViewHolder
         RecyclerView recyclerView = holder.recyler;
         recyclerView.setLayoutManager(new LinearLayoutManager(mamieActivity));
 
-        MamieAdapter adapter = new MamieAdapter(itemsSoluce.get(myPosition));
+        //itemsSoluce.get(myPosition) //Inside mamieAdapte(..)
+        MamieAdapter adapter = new MamieAdapter((JSONArray) combinedList.get(myPosition).get(2));
         recyclerView.setAdapter(adapter);
         recyclerView.setVisibility(View.VISIBLE);
 
     }
 }
+
+
